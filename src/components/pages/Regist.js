53e3e6722c4e5a6493a333590c2a9c71b78582eb
Niranjan <Niranjan.vibgyorweb@gmail.com>
2021-12-21 10:06:39 +0530

    Login Commit

--- src/components/pages/Regist.js
+++ src/components/pages/Regist.js
@@ -1,7 +1,5 @@
-import react from 'react'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import CrudApi from './CrudApi'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MyNavbar from '../layout/MyNavbar';
