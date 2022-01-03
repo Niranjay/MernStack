80f1c031a1cb3ad071122a1f4adb551c9dddefdf
Niranjan <Niranjan.vibgyorweb@gmail.com>
2022-01-03 13:33:48 +0530

    Resend Function

--- src/components/pages/ResetPass.js
+++ src/components/pages/ResetPass.js
@@ -1,43 +1,69 @@
 // import { React, useEffect, createContext, useContext, useState } from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import { useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MyNavbar from '../layout/MyNavbar';
 import { NavLink } from 'react-router-dom';
 
+
 function ResetPass(){
     const [email, setEmail] = useState("");
     const [password, setPass] = useState("");
     const [otp, setOtp] = useState("");
+    const [disbl, setdisbl] = useState("disable")
+    const [hid , setHid] =useState("hidden")
+    const [showpass, setShowpass]= useState("hidden")
+    const [emailHide, setEmailHide] = useState("")
+    const [otpHide, setOtpHide] = useState("hedden")
+
+    const [tCont, setTcont] = useState(20);
+    
+    useEffect(()=>{
+        if(tCont>0 ){
+            const timer = tCont > 0 && setInterval(()=> setTcont(tCont-1), 1000);
+            clearInterval(timer);
+        }
+
+    })
+
+
 
     // function call on GetOTP button
-    const getOTP = async (e)=> {
+    const getOTP = async (e) => {
         e.preventDefault();
-        let data={email}
+        let data = {email} 
         
-        fetch("http://localhost:5000/sendotp",{ method :'POST', headers:{'Accept':'application/json', 'Content-Type':'application/json'},
-                    body:JSON.stringify(data)} 
-                    ).then((result)=>{
-        // navigate('/users/show')          //use for redirect to CrudApi page
-        console.log(getOTP)
-        if(getOTP){
-            toast.success("OTP Send sucessfully...",
-            {
-                position:"top-center"
-            });
-        }    
-        else{
-            toast.error("User Not Found",
-            {
-                position:"top-center"
-            });
+        if (email) {
+                    fetch("http://localhost:5000/sendotp", {
+                    method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data)})
+                    .then((result) => {
+                                        console.log(result.otp)
+                                        if (getOTP) {
+                                        toast.success("OTP Send sucessfully...",
+                                        {
+                                        position: "top-center"
+                                        });
+                                    setdisbl("")
+                                    setHid("")
+                                    setEmailHide("hidden")
+                                    setOtpHide("")
+                               }    
+                                else {
+                                    toast.error("User Not Found",
+                                    {
+                                     position: "top-center"
+                                    });}}
+                    )}
+                    else{
+                        toast.error("kindly enter email Id...",
+                                        {
+                                        position: "top-center"
+                                        });
+                    }
         }
-        
-      
-    })
-    
-}
 
 
 // Fuction on Submit
@@ -75,6 +101,20 @@ function ResetPass(){
 }
 
 
+
+function showfull(e){
+    e.preventDefault();
+ setHid("hidden")
+ setShowpass("")
+ setdisbl("disable")
+
+}
+function timer(){
+    const times = new Date().getTime() + 300*10000
+}
+
+
+
     return (
         <>
         <MyNavbar />
@@ -85,15 +125,20 @@ function ResetPass(){
             <form>
                 <div className="form-group">
 
-                    <input type="text" className='form-control' placeholder="Email address" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type="text" className='form-control' hidden={emailHide} placeholder="Email address" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                     
-                    <br/><button className="btn btn-primary btn-block" onClick={getOTP} >GetOTP</button>
-                    <input type="text" className='form-control' placeholder="Otp" name="OTP"  onChange={(e) => { setOtp(e.target.value) }} />
-                    <br/><input type="password" className='form-control' placeholder="New Password" name="newPassword"  onChange={(e) => { setPass(e.target.value) }} />
+                    <br/><button className="btn btn-primary btn-block" hidden={emailHide} onClick={getOTP} >GetOTP</button>
+                    
+                    <input type="text" className='form-control' placeholder="Otp" hidden={otpHide} disabled={disbl} name="OTP"  onChange={(e) => { setOtp(e.target.value) }} />
+                    <p ></p>
+                    < h2 style={{textAlign: 'left'}}>{tCont}</h2><br/>
+                    <br/><button onClick={getOTP} hidden={otpHide}>Resend Otp</button>
+                    <br/><input type="password" className='form-control'hidden={showpass} placeholder="New Password" name="newPassword"  onChange={(e) => { setPass(e.target.value) }} />
                     <br/>
                 </div>
             
-               <center> <button className="btn btn-primary btn-block" onClick={resetPass} >Submit</button></center>
+               <center> <button className="btn btn-primary btn-block" hidden={hid} onClick={showfull} >Next</button></center>
+               <center> <button className="btn btn-primary btn-block" hidden={showpass} onClick={resetPass} >Submit</button></center>
                 <p ><NavLink to="/">GoBack</NavLink></p>
             </form>
             
